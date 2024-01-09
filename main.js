8a5456590218cd70e176cfba1fe41aad5cf9b140
Reilly Middlebrooks <reimidd7@uw.edu>
2024-01-09 15:03:53 -0800

    Added a walking hulk sprite

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./hulk2.png");
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
+
+	gameEngine.addEntity(new Hulk(gameEngine));
+
 
 	gameEngine.init(ctx);
 
